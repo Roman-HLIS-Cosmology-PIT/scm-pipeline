88cb338d57fef26e1a36e2c0e36c431bf068ab09
aguinot <axel.guinot.astro@gmail.com>
2026-02-06 14:30:35 -0500

    Add example to run

---
+++ examples/example_multi_input/README.md
@@ -0,0 +1,13 @@
+# Running example with Multi-Input
+
+To run this example you need to install the `scm-pipeline` but you also need the modified version of ceci that can be find [here](https://github.com/Roman-HLIS-Cosmology-PIT/ceci/tree/multi_input) and installed by:
+
+```bash
+pip install git+https://github.com/Roman-HLIS-Cosmology-PIT/ceci@multi_input
+```
+
+Then you can run the example from this directory by doing:
+
+```bash
+ceci ceci_dummy_stage.yml
+```
--- src/scm_pipeline/dummy_stage.py
+++ src/scm_pipeline/dummy_stage.py
@@ -1,6 +1,10 @@
 from .base_stage import PipelineStage
 from .data_types import TextFile
 
+from time import sleep
+
+import numpy as np
+
 
 class DummyStage(PipelineStage):
     name = "DummyStage"
@@ -25,3 +29,67 @@ class DummyStage(PipelineStage):
             with open(filename, "w") as f:
                 for n in numbers:
                     f.write(f"{n * my_config['multiplier']}\n")
+
+
+class DummyStageWait(PipelineStage):
+    name = "DummyStageWait"
+    inputs = [("numbers", TextFile)]
+    outputs = [("new_numbers", TextFile)]
+    config_options = {"multiplier": float}
+
+    def run(self):
+        # Retrieve configuration:
+        my_config = self.config
+        print("Here is my configuration :", my_config)
+
+        for inp, _ in self.inputs:
+            filename = self.get_input(inp)
+            print(f"    numbers reading from {filename}")
+            with open(filename) as f:
+                numbers = [float(line.strip()) for line in f]
+
+        np.random.seed(int(numbers[0]))  # Seed the random number generator for reproducibility
+        sleep_time = np.random.uniform(10, 20)
+        print(f"Sleeping for {sleep_time:.1f} seconds...")
+        sleep(sleep_time)
+
+        for out, _ in self.outputs:
+            filename = self.get_output(out)
+            print(f"    new_numbers writing to {filename}")
+            with open(filename, "w") as f:
+                for n in numbers:
+                    f.write(f"{n * my_config['multiplier']}\n")
+
+
+class DummyStageWait2(PipelineStage):
+    name = "DummyStageWait2"
+    inputs = [("numbers", TextFile), ("new_numbers", TextFile)]
+    outputs = [("new_numbers2", TextFile)]
+    config_options = {"multiplier": float}
+
+    def run(self):
+        # Retrieve configuration:
+        my_config = self.config
+        print("Here is my configuration :", my_config)
+
+        filename = self.get_input("numbers")
+        print(f"    numbers reading from {filename}")
+        with open(filename) as f:
+            numbers = [float(line.strip()) for line in f]
+
+        filename = self.get_input("new_numbers")
+        print(f"    new_numbers reading from {filename}")
+        with open(filename) as f:
+            new_numbers = [float(line.strip()) for line in f]
+
+        np.random.seed(int(new_numbers[0]))  # Seed the random number generator for reproducibility
+        sleep_time = np.random.uniform(10, 20)
+        print(f"Sleeping for {sleep_time:.1f} seconds...")
+        sleep(sleep_time)
+
+        for out, _ in self.outputs:
+            filename = self.get_output(out)
+            print(f"    new_numbers writing to {filename}")
+            with open(filename, "w") as f:
+                for i in range(len(numbers)):
+                    f.write(f"{(new_numbers[i] + numbers[i]) * my_config['multiplier']}\n")
